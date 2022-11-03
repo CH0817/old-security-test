02fa5d9526e62c34306a18639a993582b5b3845b
rex <yu.chenhang@gmail.com>
2022-11-03 15:01:32 +0800

    OAuth2 for in-memory user

---
+++ src/main/java/tw/com/rex/oldsecuritytest/OldSecurityTestApplication.java
@@ -0,0 +1,13 @@
+package tw.com.rex.oldsecuritytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldSecurityTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldSecurityTestApplication.class, args);
+    }
+
+}
