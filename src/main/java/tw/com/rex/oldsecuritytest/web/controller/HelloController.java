d7371a34490a64f119e48f78f542035bc684e5a9
rex <yu.chenhang@gmail.com>
2022-11-06 23:55:27 +0800

    增加 resource server

---
+++ src/main/java/tw/com/rex/oldsecuritytest/web/controller/HelloController.java
@@ -0,0 +1,17 @@
+package tw.com.rex.oldsecuritytest.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class HelloController {
+
+    @PostMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello~");
+    }
+
+}
