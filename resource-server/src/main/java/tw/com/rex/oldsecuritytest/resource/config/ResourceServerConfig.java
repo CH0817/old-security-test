32d65bf442980a6e9df0c6c4d60b880d54f626e8
rex <yu.chenhang@gmail.com>
2022-11-07 22:42:15 +0800

    增加 RemoteTokenServers 設定

--- resource-server/src/main/java/tw/com/rex/oldsecuritytest/resource/config/ResourceServerConfig.java
+++ resource-server/src/main/java/tw/com/rex/oldsecuritytest/resource/config/ResourceServerConfig.java
@@ -1,10 +1,12 @@
 package tw.com.rex.oldsecuritytest.resource.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @Configuration
 @EnableResourceServer
@@ -12,11 +14,21 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/test/hello").access("#oauth2.hasAnyScope('all')");
+        http.csrf().disable().authorizeRequests().anyRequest().access("#oauth2.hasAnyScope('all')");
     }
 
     @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+    public void configure(ResourceServerSecurityConfigurer resources) {
         resources.resourceId("oauth-resource");
     }
+
+    @Bean
+    public RemoteTokenServices remoteTokenServices() {
+        RemoteTokenServices services = new RemoteTokenServices();
+        services.setCheckTokenEndpointUrl("http://localhost:8880/oauth/check_token");
+        services.setClientId("oauth");
+        services.setClientSecret("oauth");
+        return services;
+    }
+
 }
